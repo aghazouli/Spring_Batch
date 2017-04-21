41313dc602a0f728a44d75ff2853c7241223752f
Your Name <you@example.com>
2017-04-21 16:42:18 +0200

    commit new files

---
+++ src/main/java/com/aghazouli/App.java
@@ -0,0 +1,13 @@
+package com.aghazouli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
